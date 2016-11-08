d5509fdbfcc9c719bc37ce7ef5a7fc904464f026
nadeshseen <nadeshseen@gmail.com>
2016-11-08 18:22:55 +0530

    edited test.c

--- test.c
+++ test.c
@@ -1,16 +1,1 @@
-#include<stdio.h>
-int main()
-{
-	int num[100];
-	i=0;
-	while(1)
-	{
-		scanf("%d",&num[i]);
-		if(num[i]==42)
-		break;
-		i++;
-	}
-	for(j=0;j<i;i++)
-		printf("%d\n",num);
-	return 0;
-}
+
